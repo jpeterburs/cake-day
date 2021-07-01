04beb7a52ed125eb3e2d0ce69bb15e5b2d3f5d37
stealthyV1per <j.peterburs@protonmail.com>
2021-07-01 22:23:39 +0200

    Remove unused code snipets

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,13 +1,5 @@
 require 'active_record'
 
 class User < ActiveRecord::Base
-  default_scope { order(discord_id: :desc).order(server_id: :desc) }
-
   validates_presence_of :discord_id, :birthdate, :server_id
-
-  def delete
-    ActiveRecord::Base.connection.execute(
-      "DELETE FROM cake_day.users WHERE discord_id = #{discord_id} AND server_id = #{server_id}"
-    )
-  end
 end
